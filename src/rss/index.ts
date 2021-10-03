dea77910084bddf6cc72b7c5ecefefc40af3d62e
Freddie Kins <tsfreddiewang@gmail.com>
2021-10-03 21:07:35 +0800

    speed up map notification

--- src/rss/index.ts
+++ src/rss/index.ts
@@ -5,7 +5,7 @@ import { recordFeed } from './record';
 export const feederStart = () => {
   const feeder = new RssFeeder(10000);
   feeder.startFeed('https://ddnet.tw/status/records/feed/', 'record', 30000, 10000);
-  feeder.startFeed('https://ddnet.tw/releases/feed/', 'map', 60000 * 30, 15000);
+  feeder.startFeed('https://ddnet.tw/releases/feed/', 'map', 300000, 15000);
   feeder.register('record', recordFeed);
   feeder.register('map', mapFeed);
 };
